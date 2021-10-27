a8587a7db3da502155240b217a0d9ac36a46ff62
AizeRs <NAKorshunov@mail.ru>
2021-10-27 20:38:12 +0500

    debug CofeMachine

--- src/coffeemachine/CofeMachine.java
+++ src/coffeemachine/CofeMachine.java
@@ -1,25 +1,21 @@
 package coffeemachine;
 
 public class CofeMachine {
+
     public enum Status {
         ACTION, KIND, FILL
     }
-    int[] ingridience = new int[]{400, 540, 120, 550, 9};
-    int waterIndex = 0;
-    int milkIndex = 1;
-    int beansIndex = 2;
-    int moneyIndex = 3;
-    int cupIndex = 4;
-    int[] espresso = new int[]{250, 0, 16, 4};
-    int[] latte = new int[]{350, 75, 20, 7};
-    int[] cappuchino = new int[]{200, 100, 12, 6};
-    int[] filling = new int[5];
-    int water = 0;
-    int milk = 0;
-    int beans = 0;
-    int cups = 0;
-    int j = 0;
-    boolean working = true;
+
+    public enum Cofe{
+        espresso, latte, cappuccino
+    }
+
+    int water = 400;
+    int milk = 540;
+    int beans = 120;
+    int cups = 9;
+    int money = 550;
+
     Status status = Status.ACTION;
     public void work(String inner) {
         switch (this.status) {
@@ -33,150 +29,82 @@ public class CofeMachine {
                     System.out.println("Write how many ml of water you want to add: ");
                 }
                 else if(inner.equals("remaining")){
-                    remaining(this.ingridience, waterIndex, milkIndex, beansIndex, cupIndex, moneyIndex);
+                    remaining();
                 }
                 else if(inner.equals("take")){
-                    this.ingridience = take(this.ingridience, moneyIndex);
-                }
-                else if(inner.equals("exit")){
-                    this.working = false;
+                    take();
                 }
                 break;
             case KIND:
-                this.ingridience = buy(this.ingridience, waterIndex, milkIndex, beansIndex, moneyIndex,
-                        cupIndex, espresso, latte, cappuchino, inner);
+                buy(inner);
                 this.status = Status.ACTION;
                 break;
-            case FILL:
-                if(this.j == 0){
-                    for(int i = 0; i < inner.length(); i++){
-                        this.water += (inner.charAt(i) - 48) * Math.pow(10, inner.length() - 1 - i);
-                    }
-                    this.filling[waterIndex] = this.water;
-                    this.j++;
-                    System.out.println("Write how many ml of milk you want to add: ");
-                    return;
-                }
-                if(this.j == 1){
-                    for(int i = 0; i < inner.length(); i++){
-                        this.milk += (inner.charAt(i) - 48) * Math.pow(10, inner.length() - 1 - i);
-                    }
-                    this.filling[milkIndex] = this.milk;
-                    this.j++;
-                    System.out.println("Write how many grams of coffee beans you want to add: ");
-                    return;
-                }
-                if(this.j == 2){
-                    for(int i = 0; i < inner.length(); i++){
-                        this.beans += (inner.charAt(i) - 48) * Math.pow(10, inner.length() - 1 - i);
-                    }
-                    this.filling[beansIndex] = this.beans;
-                    this.j++;
-                    System.out.println("Write how many disposable cups of coffee you want to add:");
-                    return;
-                }
-                if(this.j == 3){
-                    for(int i = 0; i < inner.length(); i++){
-                        this.cups += (inner.charAt(i) - 48) * Math.pow(10, inner.length() - 1 - i);
-                    }
-                    this.filling[cupIndex] = this.cups;
-                    this.j = 0;
-                    this.ingridience = fill(this.ingridience, waterIndex, milkIndex, beansIndex, cupIndex, this.filling);
-                    this.status = Status.ACTION;
-                }
         }
     }
-    public static int[] buy(int[] ingridience, int waterIndex, int milkIndex, int beansIndex, int moneyIndex, int cupIndex,
-                            int[] espresso, int[] latte, int[] cappuchino, String kind) {
+    public void make(Cofe coffe){
+        System.out.println("I have enough resources, making you a coffee!");
+        switch (coffe){
+            case espresso:
+                makeCofe(250, 0, 16, 4);
+                break;
+            case latte:
+                makeCofe(350, 75, 20, 7);
+                break;
+            case cappuccino:
+                makeCofe(200, 100, 12, 6);
+                break;
+        }
+    }
+    public void makeCofe(int nWater, int nMilk, int nBeans, int money){
+        if(nWater <= this.water) {
+            if (nMilk <= this.milk) {
+                if (nBeans <= this.beans) {
+                    if (this.cups != 0) {
+                        this.water -= nWater;
+                        this.milk -= nMilk;
+                        this.beans -= nBeans;
+                        this.money += money;
+                        this.cups--;
+                    } else {
+                        System.out.println("Sorry, not enough cups!");
+                    }
+                } else {
+                    System.out.println("Sorry, not enough beans!");
+                }
+            } else {
+                System.out.println("Sorry, not enough milk!");
+            }
+        }
+        else{
+            System.out.println("Sorry, not enough water!");
+        }
+    }
+
+    public void buy(String kind) {
         switch (kind) {
             case "1":
-                if (espresso[waterIndex] <= ingridience[waterIndex]) {
-                    if (espresso[beansIndex] <= ingridience[beansIndex]){
-                        System.out.println("I have enough resources, making you a coffee!");
-                        ingridience[moneyIndex] += espresso[moneyIndex];
-                        ingridience[milkIndex] -= espresso[milkIndex];
-                        ingridience[waterIndex] -= espresso[waterIndex];
-                        ingridience[beansIndex] -= espresso[beansIndex];
-                        ingridience[cupIndex]--;
-                    }
-                    else{
-                        System.out.println("Sorry, not enough beans!");
-                    }
-                }
-                else{
-                    System.out.println("Sorry, not enough water!");
-                }
+                make(Cofe.espresso);
                 break;
             case "2":
-                if (latte[waterIndex] <= ingridience[waterIndex]) {
-                    if(latte[milkIndex] <= ingridience[milkIndex]){
-                        if (latte[beansIndex] <= ingridience[beansIndex]){
-                            System.out.println("I have enough resources, making you a coffee!");
-                            ingridience[moneyIndex] += latte[moneyIndex];
-                            ingridience[milkIndex] -= latte[milkIndex];
-                            ingridience[waterIndex] -= latte[waterIndex];
-                            ingridience[beansIndex] -= latte[beansIndex];
-                            ingridience[cupIndex]--;
-                        }
-                        else{
-                            System.out.println("Sorry, not enough beans!");
-                        }
-                    }
-                    else{
-                        System.out.println("Sorry, not enough milk!");
-                    }
-                }
-                else{
-                    System.out.println("Sorry, not enough water!");
-                }
+                make(Cofe.latte);
                 break;
             case "3":
-                if (cappuchino[waterIndex] <= ingridience[waterIndex]) {
-                    if(cappuchino[milkIndex] <= ingridience[milkIndex]){
-                        if (cappuchino[beansIndex] <= ingridience[beansIndex]){
-                            System.out.println("I have enough resources, making you a coffee!");
-                            ingridience[moneyIndex] += cappuchino[moneyIndex];
-                            ingridience[milkIndex] -= cappuchino[milkIndex];
-                            ingridience[waterIndex] -= cappuchino[waterIndex];
-                            ingridience[beansIndex] -= cappuchino[beansIndex];
-                            ingridience[cupIndex]--;
-                        }
-                        else{
-                            System.out.println("Sorry, not enough beans!");
-                        }
-                    }
-                    else{
-                        System.out.println("Sorry, not enough milk!");
-                    }
-                }
-                else{
-                    System.out.println("Sorry, not enough water!");
-                }
+                make(Cofe.cappuccino);
                 break;
-            case "4":
-                return ingridience; default: System.out.println("Unknown coffee");
-                break;
+            default:
+                System.out.println("Unknown symbol.");
         }
-        return ingridience;
     }
-    public static int[] fill(int[] ingridience, int waterIndex, int milkIndex, int beansIndex, int cupIndex, int[] filling){
-        ingridience[waterIndex] += filling[waterIndex];
-        ingridience[milkIndex] += filling[milkIndex];
-        ingridience[beansIndex] += filling[beansIndex];
-        ingridience[cupIndex] += filling[cupIndex];
-        return ingridience;
+    public void take(){
+        System.out.println("I gave you $" + this.money);
+        this.money = 0;
     }
-    public static int[] take(int[] ingridience, int moneyIndex){
-        System.out.println("I gave you $" + ingridience[moneyIndex]);
-        ingridience[moneyIndex] = 0;
-        return ingridience;
-    }
-    public static void remaining(int[] ingridience, int waterIndex, int milkIndex, int beansIndex, int cupIndex, int moneyIndex){
+    public void remaining(){
         System.out.println("The coffee machine has:");
-        System.out.println(ingridience[waterIndex] + " ml of water");
-        System.out.println(ingridience[milkIndex] + " ml of milk");
-        System.out.println(ingridience[beansIndex] + " g of coffee beans");
-        System.out.println(ingridience[cupIndex] + " disposable cups");
-        System.out.println("$ " + ingridience[moneyIndex] + " of money");
+        System.out.println(this.water + " ml of water");
+        System.out.println(this.milk + " ml of milk");
+        System.out.println(this.beans + " g of coffee beans");
+        System.out.println(this.cups + " disposable cups");
+        System.out.println("$ " + this.money + " of money");
     }
 }
--- src/coffeemachine/Main.java
+++ src/coffeemachine/Main.java
@@ -5,11 +5,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         CofeMachine cofe1 = new CofeMachine();
-        while(cofe1.working){
+        String action;
+        boolean working = true;
+        while(working){
             switch(cofe1.status){
                 case ACTION:
                     System.out.println("Write action (buy, fill, take, remaining, exit):");
-                    cofe1.work(in.nextLine());
+                    action = in.nextLine();
+                    if(action.equals("exit")){
+                        working = false;
+                        break;
+                    }
+                    cofe1.work(action);
                     break;
                 case KIND:
                     cofe1.work(in.nextLine());
